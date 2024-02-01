4f8ba2f7bbf1fa93379e55f269025c41aae2cbd5
krousselet <krous74@gmail.com>
2024-02-01 12:18:01 +0100

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,93 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Navbar :pages="pages" :activePage="activePage" :isHamburgerActive="isHamburgerActive"
+    @toggle-hamburger="toggleHamburger" :groups="groups"></Navbar>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Navbar from './components/Navbar.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Navbar,
+  },
+  methods: {
+    toggleHamburger() {
+      this.isHamburgerActive = !this.isHamburgerActive;
+    }
+  },
+  data() {
+    return {
+      isHamburgerActive: false,
+      activePage: 0,
+      groups: [
+        {
+          imageUrl: require('@/assets/blind_to_death_1024.svg'),
+        },
+        {
+          imageUrl: require('@/assets/kouett_pounk_white.svg'),
+        },
+      ],
+      pages: [
+        {
+          link: {
+            url: '#',
+            target: 'Home'
+          },
+          pageTitle: 'Bienvenue à la Saint Patrick !',
+          pageContent: [
+            {
+              imageUrl: require('@/assets/irish_darts_1024x1024.svg'),
+              heading: 'Skull Darts vous propose',
+              text: 'Une soirée inoubliable !',
+            }
+          ],
+        },
+        {
+          link: {
+            url: '#',
+            target: 'Details'
+          },
+          pageTitle: 'Informations :',
+          pageContent: [
+            {
+              imageUrl: '',
+              heading: '',
+              text: '',
+            }
+          ],
+        }
+      ]
+    }
   }
 }
 </script>
 
 <style>
+body {
+  background-color: rgba(0, 0, 0, 0.959) !important;
+}
+
+h1,
+p {
+  font-family: 'Irish Grover', system-ui;
+  color: gold;
+}
+
+img {
+  border-radius: 7px;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+
+}
+
+#details p {
+  font-size: 24px;
 }
 </style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,84 @@
+<template>
+    <nav class="navbar navbar-expand-lg bg-body-transparent">
+        <div class="container-fluid">
+            <img src="#" alt="logo" srcset="">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item" v-for="(page, pageIndex) in pages" :key="pageIndex">
+                        <a class="nav-link text-success" aria-current="page" href="#"
+                            @click.prevent="activePage = pageIndex" :class="{ active: activePage === pageIndex }">{{
+                                page.link.target }}</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <div id="container">
+        <h1 class="m-5">{{ pages[activePage].pageTitle }}</h1>
+        <div id="details" v-for="(item, pageDetails) in pages[activePage].pageContent" :key="pageDetails">
+            <img class="img-fluid" :src="item.imageUrl" :alt="'image d\'un logo'" id="irish_darts">
+            <p class="m-5">{{ item.heading }}</p>
+            <p>{{ item.text }}</p>
+            <div id="groups" class="m-5">
+                <ul id="list">
+                    <li v-for="group in groups"><img class="img-fluid" :src="group.imageUrl" alt="" srcset=""></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    emits: ['toggle-hamburger'],
+    methods: {
+        toggleHamburger() {
+            this.$emit('toggle-hamburger');
+        }
+    },
+    props: {
+        activePage: Number,
+        isHamburgerActive: Boolean,
+        groups: {
+            type: Object,
+            default(rawProps) {
+                return (() => [])
+            }
+        },
+        pages: {
+            type: Object,
+            default(rawProps) {
+                return (() => [
+
+                ])
+            }
+        }
+    }
+}
+</script>
+
+<style scoped>
+.active {
+    color: gold !important;
+}
+
+#irish_darts {
+    box-shadow: 0 0 10px green;
+}
+
+#list {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+    align-items: center;
+}
+
+#list li img {
+    max-width: 200px;
+    max-height: 100px;
+}
+</style>
